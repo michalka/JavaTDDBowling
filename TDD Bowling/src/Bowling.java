87aaf0fd90b784bc0fcfefce1bbf21300e2db77a
Matt <matthew.michalka@gmail.com>
2014-01-06 23:55:17 -0600

    Initial method and test stub

---
+++ TDD Bowling/src/Bowling.java
@@ -0,0 +1,12 @@
+
+public class Bowling {
+
+	public Bowling() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int CalculateScore() {
+		return 0;
+	}
+
+}
---
+++ TDD Bowling/src/test/BowlingTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BowlingTest {
+
+	@Test
+	public void testCalculateScore() {
+		fail("Not yet implemented");
+	}
+
+}
