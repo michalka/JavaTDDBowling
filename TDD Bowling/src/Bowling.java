355f7ed2028be0770907b120c0b74a2dfc99ac58
Matt <matthew.michalka@gmail.com>
2014-01-07 14:00:15 -0600

    Green

--- TDD Bowling/src/Bowling.java
+++ TDD Bowling/src/Bowling.java
@@ -7,11 +7,23 @@ public class Bowling {
 	
 	public static int calculateScore(String frames) {
 		int score = 0;
+		boolean spareFlag = false;
 		String[] game = new String[10];
 		game = frames.split(" ");
 		for(String frame : game){
-			score += Character.getNumericValue(frame.charAt(0));
-			score += Character.getNumericValue(frame.charAt(1));
+			int tempScore = Character.getNumericValue(frame.charAt(0));
+			if(spareFlag){
+				score += tempScore;
+				spareFlag = false;
+			}
+			if(frame.charAt(1) == '/'){
+				tempScore = 10; 
+				spareFlag = true;
+			}
+			else{
+				tempScore += Character.getNumericValue(frame.charAt(1));
+			}
+			score += tempScore;
 		}
 		return score;
 	}
