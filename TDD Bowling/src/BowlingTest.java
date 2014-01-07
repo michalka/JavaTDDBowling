5bf1d4d86ae262fb2453f1a7208cd050a7ccd08f
Matt <matthew.michalka@gmail.com>
2014-01-07 10:38:47 -0600

    Red: Test for two frames

--- TDD Bowling/src/BowlingTest.java
+++ TDD Bowling/src/BowlingTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BowlingTest {
-
 	@Test
-	public void testCalculateScore() {
+	public void simpleFrame(){
 		assertEquals("A frame with two rolls of one should be 2",2,Bowling.calculateScore("11"));
 	}
-
+	@Test
+	public void twoFrames(){
+		assertEquals("Two frames with two rolls of one should be 4",4,Bowling.calculateScore("11 11"));
+	}
 }
