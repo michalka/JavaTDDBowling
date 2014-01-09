cb8b805aa78b8a216ce6cc8dc5f93d771f33b1e5
Matt <matthew.michalka@gmail.com>
2014-01-09 12:52:49 -0600

    Red: Bonus roll

--- TDD Bowling/src/BowlingTest.java
+++ TDD Bowling/src/BowlingTest.java
@@ -1,5 +1,3 @@
-
-
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -29,6 +27,21 @@ public class BowlingTest {
 	public void twoStrikes(){
 		assertEquals("A strike score should include the next two balls, even if it's in different frames",35,Bowling.calculateScore("X X 11"));
 	}
+	
+	@Test
+	public void bonusRoll(){
+		assertEquals("A spare in the tenth frame should get one bonus roll",56,Bowling.calculateScore("01 02 03 04 05 06 07 08 09 1/1"));
+	}
+	
+	/*@Test
+	public void bonusStrike(){
+		assertEquals("A strike in the tenth frame should get two bonus rolls",58,Bowling.calculateScore("01 02 03 04 05 06 07 08 09 X12"));
+	}
+	
+	@Test
+	public void bonusTurkey(){
+		assertEquals("A turkey in the tenth frame should provide a score of 30 for the frame",75,Bowling.calculateScore("01 02 03 04 05 06 07 08 09 XXX"));
+	}*/
 	
 	/*@Test
 	public void fullGame(){
