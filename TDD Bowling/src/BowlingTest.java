01044cc06a0adc7c6426141fa4cd41a752a4c692
Matt <matthew.michalka@gmail.com>
2014-01-09 13:26:49 -0600

    Red: Strike in final frame

--- TDD Bowling/src/BowlingTest.java
+++ TDD Bowling/src/BowlingTest.java
@@ -33,12 +33,12 @@ public class BowlingTest {
 		assertEquals("A spare in the tenth frame should get one bonus roll",56,Bowling.calculateScore("01 02 03 04 05 06 07 08 09 1/1"));
 	}
 	
-	/*@Test
+	@Test
 	public void bonusStrike(){
 		assertEquals("A strike in the tenth frame should get two bonus rolls",58,Bowling.calculateScore("01 02 03 04 05 06 07 08 09 X12"));
 	}
 	
-	@Test
+	/*@Test
 	public void bonusTurkey(){
 		assertEquals("A turkey in the tenth frame should provide a score of 30 for the frame",75,Bowling.calculateScore("01 02 03 04 05 06 07 08 09 XXX"));
 	}*/
