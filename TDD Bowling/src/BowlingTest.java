3355c0c7e8c74a5ce7d216f05babd17dad419f1d
Matt <matthew.michalka@gmail.com>
2014-01-09 13:47:50 -0600

    Additional tests, all already passing.

--- TDD Bowling/src/BowlingTest.java
+++ TDD Bowling/src/BowlingTest.java
@@ -43,8 +43,29 @@ public class BowlingTest {
 		assertEquals("A turkey in the tenth frame should provide a score of 30 for the frame",75,Bowling.calculateScore("01 02 03 04 05 06 07 08 09 XXX"));
 	}
 	
-	/*@Test
+	@Test
 	public void fullGame(){
-		assertEquals("This is a full game",14,Bowling.calculateScore("X 11 5/ 90 X X "));
-	}*/
+		assertEquals("This is a full game",132,Bowling.calculateScore("X 11 5/ 90 X X 35 72 0/ 3/9"));
+	}
+	
+	//Supplied Test Data 
+	@Test
+	public void test1(){
+		assertEquals("Test 1",20,Bowling.calculateScore("11 11 11 11 11 11 11 11 11 11"));
+	}
+	
+	@Test
+	public void test2(){
+		assertEquals("Test 2",22,Bowling.calculateScore("11 5/ 50 00 00 00 00 00 00 00"));
+	}
+	
+	@Test
+	public void test3(){
+		assertEquals("Test 3",24,Bowling.calculateScore("00 X 52 00 00 00 00 00 00 00"));
+	}
+	
+	@Test
+	public void test4(){
+		assertEquals("Test 4",30,Bowling.calculateScore("00 00 00 00 00 00 00 00 00 XXX"));
+	}
 }
